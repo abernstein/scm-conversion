72b46153292d8ed8ccc465b603bb047709e3b0bb
Aaron Bernstein <aaronbernstein@cox.net>
2014-02-27 18:10:51 -0700

    Update svn2git-repo.sh
    Continuing to refactor to be able to pull authors from the SVN URL instead of a local checkout.

--- svn2git-repo.sh
+++ svn2git-repo.sh
@@ -17,7 +17,6 @@
 
 PREFIX=svn
 AUTHOR_FILE=authors.txt
-SVN_REPO_DIR=/www 
 SVN_REPO_URL=https://svn.repo.url/repo
 CLONE_DIR=~/
 GIT_REPO_NAME=myrepository.git
@@ -25,24 +24,24 @@ GIT_REMOTE=git@my-git-server:myrepository.git
 
 ###################################################
 # Create an authors file from the current svn repo
-if [ -n $SVN_REPO_DIR ]; then
-
-  cd $SVN_REPO_DIR
-
-  svn log --xml | grep -P "^<author" | sort -u | \
-    perl -pe 's/<author>(.*?)<\/author>/$1 = /' > $AUTHOR_FILE
+ if [ -n $SVN_REPO_URL ]; then
+  if [ -e ${AUTHOR_FILE} ]; then
+    echo "${AUTHOR_FILE} already exists - not overwriting"
+  else
+    SVN_ROOT=`svn info "${SVN_REPO_URL}" | grep '^Repository.Root' | sed -e 's/^Repository.Root: //'`
+    svn log -q $SVN_ROOT | \
+      awk -F '|' '/^r/ {sub("^ ", "", $2); sub(" $", "", $2); print $2" = "$2" "}' | \
+      sort -u > $AUTHOR_FILE
+  fi
 fi
 ####################################
 # Create empty repo on local machine
-
 cd $CLONE_DIR
-
-git svn clone --prefix=$PREFIX/ \
-  --authors-file=$AUTHOR_FILE $SVN_REPO_URL $GIT_REPO_NAME
+git svn clone --prefix=$PREFIX/ --authors-file=$AUTHOR_FILE \
+  $SVN_REPO_URL $GIT_REPO_NAME
 
 #############################
 # Confirm and reset the head
-
 cd $REPO_NAME
 git branch -a -v | cut -c1-60
 git reset --hard $PREFIX/trunk
@@ -50,19 +49,22 @@ git branch -a -v | cut -c1-60
 
 ########################################
 # Create tags for each remote tag/branch
-
 git for-each-ref refs/remotes/$PREFIX/tags | \
   cut -d / -f 5- | grep -v @ \
-  while read tagname; do \ 
-    git tag "$tagname" "$PREFIX/tags/$tagname"; \
-    git branch -r -d "$PREFIX/tags/$tagname"; \
+  while read tagname; do \
+    if [ -n $tagname ]; then
+      git tag "$tagname" "$PREFIX/tags/$tagname"; \
+      git branch -r -d "$PREFIX/tags/$tagname"; \
+    fi
   done
 
 git for-each-ref refs/remotes/$PREFIX | \
   cut -d / -f 4- | grep -v @ | \
   while read branchname; do \
-    git branch "$branchname" "refs/remotes/$PREFIX/$branchname"; \
-    git branch -r -d "$branchname"; \
+    if [ -n $branchname ]; then
+      git branch "$branchname" "refs/remotes/$PREFIX/$branchname"; \
+      git branch -r -d "$branchname"; \
+    fi
   done
 
 ################################
